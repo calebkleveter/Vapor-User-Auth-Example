805c72f27357c6ca45e86aa46ff73620b4128393
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-01-26 15:41:37 +0000

    Sources/App/main.swift - Added LoginController routes to droplet

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,6 +5,9 @@ let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider.self)
 drop.preparations.append(User.self)
 
+let loginController = LoginController()
+loginController.addRoutes(to: drop)
+
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
