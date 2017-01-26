c7e75b0c3cb19465ad4b5fdb1824916098bf965e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-01-26 15:27:07 +0000

    Sources/App/Models/User.swift - Added method body to User.authenticate

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -46,6 +46,31 @@ extension User: Preparation {
 }
 
 extension User: Auth.User {
-    static func authenticate(credentials: Credentials) throws -> Auth.User {}
+    static func authenticate(credentials: Credentials) throws -> Auth.User {
+        switch credentials {
+        case let id as Identifier:
+            guard let user = try User.find(id.id) else {
+                throw Abort.custom(status: .forbidden, message: "Invalid user identifier.")
+            }
+            
+            return user
+            
+        case let usernamePassword as UsernamePassword:
+            let fetchedUser = try User.query().filter("username", usernamePassword.username).first()
+            guard let user = fetchedUser else {
+                throw Abort.custom(status: .networkAuthenticationRequired, message: "User does not exist")
+            }
+            if try BCrypt.verify(password: usernamePassword.password, matchesHash: fetchedUser!.password) {
+                return user
+            } else {
+                throw Abort.custom(status: .networkAuthenticationRequired, message: "Invalid user name or password.")
+            }
+            
+            
+        default:
+            let type = type(of: credentials)
+            throw Abort.custom(status: .forbidden, message: "Unsupported credential type: \(type).")
+        }
+    }
     static func register(credentials: Credentials) throws -> Auth.User {}
 }
