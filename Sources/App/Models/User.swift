883bbe642828540f81894d0042c7700c8a7e2eda
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-01-26 15:20:45 +0000

    Sources/App/Models/User.swift - Created User class

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,45 @@
+import Vapor
+import VaporPostgreSQL
+import Fluent
+
+final class User {
+    var id: Node?
+    var username: String
+    var password: String
+    
+    init(username: String, password: String) {
+        self.id = nil
+        self.username = username
+        self.password = password
+    }
+    
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        username = try node.extract("username")
+        password = try node.extract("password")
+    }
+}
+
+extension User: Model {
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+            "id": id,
+            "username": username,
+            "password": password
+            ])
+    }
+}
+
+extension User: Preparation {
+    static func prepare(_ database: Database) throws {
+        try database.create("users", closure: { (user) in
+            user.id()
+            user.string("username")
+            user.string("password")
+        })
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("users")
+    }
+}
